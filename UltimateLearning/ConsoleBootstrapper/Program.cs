932781d79436c0b27773da8f2a5a16ec7a26dd06
Swaraj85 <ghoshswaraj2k@gmail.com>
2016-11-16 11:32:45 +0530

    Adding bootstrapper

---
+++ UltimateLearning/ConsoleBootstrapper/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBootstrapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Get started here !!");
+            Console.ReadLine();
+        }
+    }
+}
